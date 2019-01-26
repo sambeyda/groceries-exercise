597c97578758dc493991f9a240f8fe656f397a6a
sambeyda <46543631+sambeyda@users.noreply.github.com>
2019-01-26 17:32:11 -0500

    Assembled list of unique departments

--- groceries.py
+++ groceries.py
@@ -22,6 +22,9 @@ products = [
     {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
+#
+# PRODUCTS Part 1
+#
 
 products_count = len(products)
 
@@ -30,45 +33,33 @@ print("THERE ARE " + str(products_count) + " PRODUCTS:")
 print("--------------")
 
 
+
 def sort_by_name(any_product):
     return any_product ["name"]
 sorted_products = sorted(products,key=sort_by_name)
+ 
+# for p in sorted_products:
+#     #print(p["name"]
+#     price_usd = " (${0:.2f})".format(p["price"])
+#     print(" + " + p["name"] + price_usd)
 
+#DEPARTMENT Part 2
 
+departments= [] 
+for p in products:
+    #print(p["departments"])
+    #departments.append(p["department"])
+    if p["department"] not in departments:
+        departments.append(p["department"])
 
+department_count = len (departments)
+print("--------------")
+print("THERE ARE " + str(department_count) +  " DEPARTMENTS:")
+print("--------------")
 
+for d in departments:
+    print(d)
 
-for p in sorted_products:
-    #print(p["name"]
-    price_usd = " (${0:.2f})".format(p["price"])
-    print(" + " + p["name"] + price_usd)
-
-
-
-
-# #--------------
-# THERE ARE 20 PRODUCTS:
-# --------------
-#  + All-Seasons Salt ($4.99)
-#  + Chocolate Fudge Layer Cake ($18.50)
-#  + Chocolate Sandwich Cookies ($3.50)
-#  + Cut Russet Potatoes Steam N' Mash ($4.25)
-#  + Dry Nose Oil ($21.99)
-#  + Fresh Scent Dishwasher Cleaner ($4.99)
-#  + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
-#  + Green Chile Anytime Sauce ($7.99)
-#  + Light Strawberry Blueberry Yogurt ($6.50)
-#  + Mint Chocolate Flavored Syrup ($4.50)
-#  + Overnight Diapers Size 6 ($25.50)
-#  + Peach Mango Juice ($1.99)
-#  + Pizza For One Suprema Frozen Pizza ($12.50)
-#  + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
-#  + Pure Coconut Water With Orange ($3.50)
-#  + Rendered Duck Fat ($9.99)
-#  + Robust Golden Unsweetened Oolong Tea ($2.49)
-#  + Saline Nasal Mist ($16.00)
-#  + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
-#  + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
 # --------------
 # THERE ARE 10 DEPARTMENTS:
 # --------------
